c427988a85be8fcc1adf3d540bb303f599295b84
SIRrosen <groovesethnikon@gmail.com>
2022-10-02 23:21:22 -0400

    update threshold data to be driven by standardized values

--- src/constants.js
+++ src/constants.js
@@ -11,3 +11,10 @@ export const FORMAT_THRESHOLDS = {
     [TRACKED_STATS.REB]: [4],
     [TRACKED_STATS["3PM"]]: [1],
 };
+
+export const STAT_THRESHOLDS = {
+    [TRACKED_STATS.PTS]: {10:10, 15:15, 20:20, 25:25},
+    [TRACKED_STATS.AST]: {2:2, 4:4},
+    [TRACKED_STATS.REB]: {4:4, 6:6, 8:8},
+    [TRACKED_STATS["3PM"]]: {1:1, 2:2},
+}
--- src/playerMap.js
+++ src/playerMap.js
@@ -7,46 +7,54 @@ Not addressed here:
 5) Custom stat thresholds cannot be automatic
 */
 
-import { TRACKED_STATS, FORMAT_THRESHOLDS } from "./constants";
+import { TRACKED_STATS, FORMAT_THRESHOLDS, STAT_THRESHOLDS } from "./constants";
 
 const PlayerDataMap = {
     "Jaylen Brown": {
         ctgid: 454,
         validTrackedStats: {
-            [TRACKED_STATS.PTS]: null,
-            [TRACKED_STATS["3PM"]]: null,
+            [TRACKED_STATS.PTS]: STAT_THRESHOLDS[TRACKED_STATS.PTS][15],
+            [TRACKED_STATS["3PM"]]: STAT_THRESHOLDS[TRACKED_STATS["3PM"]][1],
         },
     },
-    "Jayson Tatum": { ctgid: 4268 },
+    "Jayson Tatum": { 
+        ctgid: 4268,
+        validTrackedStats: {
+            [TRACKED_STATS.PTS]: STAT_THRESHOLDS[TRACKED_STATS.PTS][15],
+            [TRACKED_STATS.AST]: STAT_THRESHOLDS[TRACKED_STATS.AST][2],
+            [TRACKED_STATS.REB]: STAT_THRESHOLDS[TRACKED_STATS.REB][4],
+            [TRACKED_STATS["3PM"]]: STAT_THRESHOLDS[TRACKED_STATS["3PM"]][1],
+        },
+    },
     "Al Horford": {
         ctgid: 1675,
-        validTrackedStats: { [TRACKED_STATS.REB]: null },
+        validTrackedStats: { [TRACKED_STATS.REB]: STAT_THRESHOLDS[TRACKED_STATS.REB][4] },
     },
     "Marcus Smart": {
         ctgid: 3401,
-        validTrackedStats: { [TRACKED_STATS.AST]: null },
+        validTrackedStats: { [TRACKED_STATS.AST]: STAT_THRESHOLDS[TRACKED_STATS.AST][2] },
     },
     "Khris Middleton": {
         ctgid: 2495,
         validTrackedStats: {
-            [TRACKED_STATS.PTS]: 10,
-            [TRACKED_STATS.AST]: null,
-            [TRACKED_STATS["3PM"]]: null,
+            [TRACKED_STATS.PTS]: STAT_THRESHOLDS[TRACKED_STATS.PTS][10],
+            [TRACKED_STATS.AST]: STAT_THRESHOLDS[TRACKED_STATS.AST][2],
+            [TRACKED_STATS["3PM"]]: STAT_THRESHOLDS[TRACKED_STATS["3PM"]][1],
         },
     },
     "Giannis Antetokuonmpo": {
         ctgid: 96,
         validTrackedStats: {
-            [TRACKED_STATS.PTS]: 20,
-            [TRACKED_STATS.REB]: 6,
-            [TRACKED_STATS.AST]: null,
+            [TRACKED_STATS.PTS]: STAT_THRESHOLDS[TRACKED_STATS.PTS][20],
+            [TRACKED_STATS.REB]: STAT_THRESHOLDS[TRACKED_STATS.REB][6],
+            [TRACKED_STATS.AST]: STAT_THRESHOLDS[TRACKED_STATS.AST][2],
         },
     },
     "Jrue Holiday": {
         ctgid: 1647,
         validTrackedStats: {
-            [TRACKED_STATS.PTS]: 10,
-            [TRACKED_STATS.AST]: null,
+            [TRACKED_STATS.PTS]: STAT_THRESHOLDS[TRACKED_STATS.PTS][10],
+            [TRACKED_STATS.AST]: STAT_THRESHOLDS[TRACKED_STATS.AST][2],
         },
     },
 };
