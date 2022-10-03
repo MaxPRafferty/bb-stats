500b5637b6f5226520b510322713ae59ece98dbb
Max Rafferty <maxprafferty@gmail.com>
2022-10-02 22:50:05 -0400

    by team

--- src/playerMap.js
+++ src/playerMap.js
@@ -10,43 +10,47 @@ Not addressed here:
 import { TRACKED_STATS, FORMAT_THRESHOLDS } from "./constants";
 
 const PlayerDataMap = {
-    "Jaylen Brown": {
-        ctgid: 454,
-        validTrackedStats: {
-            [TRACKED_STATS.points]: null,
-            [TRACKED_STATS["three-point-makes"]]: null,
+    Boston: {
+        "Jaylen Brown": {
+            ctgid: 454,
+            validTrackedStats: {
+                [TRACKED_STATS.points]: null,
+                [TRACKED_STATS["three-point-makes"]]: null,
+            },
+        },
+        "Jayson Tatum": { ctgid: 4268 },
+        "Al Horford": {
+            ctgid: 1675,
+            validTrackedStats: { [TRACKED_STATS.rebounds]: null },
+        },
+        "Marcus Smart": {
+            ctgid: 3401,
+            validTrackedStats: { [TRACKED_STATS.assists]: null },
         },
     },
-    "Jayson Tatum": { ctgid: 4268 },
-    "Al Horford": {
-        ctgid: 1675,
-        validTrackedStats: { [TRACKED_STATS.rebounds]: null },
-    },
-    "Marcus Smart": {
-        ctgid: 3401,
-        validTrackedStats: { [TRACKED_STATS.assists]: null },
-    },
-    "Khris Middleton": {
-        ctgid: 2495,
-        validTrackedStats: {
-            [TRACKED_STATS.points]: 10,
-            [TRACKED_STATS.assists]: null,
-            [TRACKED_STATS["three-point-makes"]]: null,
+    Milwalkee: {
+        "Khris Middleton": {
+            ctgid: 2495,
+            validTrackedStats: {
+                [TRACKED_STATS.points]: 10,
+                [TRACKED_STATS.assists]: null,
+                [TRACKED_STATS["three-point-makes"]]: null,
+            },
         },
-    },
-    "Giannis Antetokuonmpo": {
-        ctgid: 96,
-        validTrackedStats: {
-            [TRACKED_STATS.points]: 20,
-            [TRACKED_STATS.rebounds]: 6,
-            [TRACKED_STATS.assists]: null,
+        "Giannis Antetokuonmpo": {
+            ctgid: 96,
+            validTrackedStats: {
+                [TRACKED_STATS.points]: 20,
+                [TRACKED_STATS.rebounds]: 6,
+                [TRACKED_STATS.assists]: null,
+            },
         },
-    },
-    "Jrue Holiday": {
-        ctgid: 1647,
-        validTrackedStats: {
-            [TRACKED_STATS.points]: 10,
-            [TRACKED_STATS.assists]: null,
+        "Jrue Holiday": {
+            ctgid: 1647,
+            validTrackedStats: {
+                [TRACKED_STATS.points]: 10,
+                [TRACKED_STATS.assists]: null,
+            },
         },
     },
 };
